a31afee6e437a09ce961d8bc12571303110ab19d
Rogério Ferreira Brito <rogerio.ferreira@ifood.com.br>
2024-07-20 23:14:32 -0300

    script injecting in given pod label

---
+++ Makefile
@@ -0,0 +1,10 @@
+docker:
+	docker build -t sinth-chaos-poc .
+	docker tag sinth-chaos-poc localhost:5000/sinth-chaos-poc:latest
+	kind load docker-image localhost:5000/sinth-chaos-poc:latest
+
+k8s-apply:
+	kubectl apply -f ./k8s/pod.yaml
+
+k8s-delete:
+	kubectl delete -f ./k8s/pod.yaml
--- pkg/main.go
+++ pkg/main.go
@@ -2,27 +2,65 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 	"time"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
+type InfoDetails struct {
+	PID int `json:"pid"`
+}
+
+type CrictlInspectResponse struct {
+	Info InfoDetails `json:"info"`
+}
+
 func main() {
+	ctx := context.Background()
+
 	fmt.Println("sinth-chaos-poc")
 
-	pids := os.Getenv("PIDS")
+	namespace := os.Getenv("NAMESPACE")
+	labelSelector := os.Getenv("LABEL_SELECTOR")
 	netInterface := os.Getenv("NETWORK_INTERFACE")
 	netemCommands := os.Getenv("NETEM_COMMANDS")
+	socketPath := os.Getenv("SOCKET_PATH")
 
-	fmt.Println("injecting...")
+	k8sClient, err := getK8sClient()
+	if err != nil {
+		fmt.Println("error on getK8sClient")
+		panic(err)
+	}
 
-	for _, pid := range strings.Split(pids, ",") {
-		inject := fmt.Sprintf("sudo nsenter -t %s -n tc qdisc replace dev %s root netem %v", pid, netInterface, netemCommands)
+	pods, err := getPods(ctx, k8sClient, namespace, labelSelector)
+	if err != nil {
+		fmt.Println("error on getK8sClient")
+		panic(err)
+	}
+
+	containerIds := getPodsContainerIds(pods)
+	pids, err := getCrioPIDs(containerIds, socketPath)
+	if err != nil {
+		fmt.Printf("error on getCrioPIDs: %s\n", err.Error())
+		panic(err)
+	}
+
+	fmt.Printf("injecting pids %v...\n", pids)
+
+	for _, pid := range pids {
+		inject := fmt.Sprintf("sudo nsenter -t %d -n tc qdisc replace dev %s root netem %v", pid, netInterface, netemCommands)
 		injectCmd := exec.Command("/bin/bash", "-c", inject)
 
-		fmt.Printf("injecting on pid %s\n", pid)
+		fmt.Printf("injecting on pid %d\n", pid)
 
 		_, _, err := runCmd(injectCmd)
 		if err != nil {
@@ -33,13 +71,13 @@ func main() {
 
 	time.Sleep(60 * time.Second)
 
-	fmt.Println("killing...")
+	fmt.Printf("killing pids %v...\n", pids)
 
-	for _, pid := range strings.Split(pids, ",") {
-		kill := fmt.Sprintf("sudo nsenter -t %s -n tc qdisc delete dev %s root", pid, netInterface)
+	for _, pid := range pids {
+		kill := fmt.Sprintf("sudo nsenter -t %d -n tc qdisc delete dev %s root", pid, netInterface)
 		killCmd := exec.Command("/bin/bash", "-c", kill)
 
-		fmt.Printf("killing on pid %s\n", pid)
+		fmt.Printf("killing on pid %d\n", pid)
 
 		_, _, err := runCmd(killCmd)
 		if err != nil {
@@ -63,4 +101,72 @@ func runCmd(cmd *exec.Cmd) (*bytes.Buffer, *bytes.Buffer, error) {
 	}
 
 	return &out, &stdErr, nil
+}
+
+func getK8sClient() (*kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return kubernetes.NewForConfig(config)
+}
+
+func getPods(ctx context.Context, clientset *kubernetes.Clientset, namespace string, labelSelector string) (*v1.PodList, error) {
+	return clientset.CoreV1().Pods(namespace).List(ctx, metav1.ListOptions{
+		LabelSelector: labelSelector,
+	})
+}
+
+func getPodsContainerIds(pods *v1.PodList) []string {
+	var containerIds []string
+	for _, pod := range pods.Items {
+		firstContainer := pod.Status.ContainerStatuses[0]
+		containerID := strings.Replace(firstContainer.ContainerID, "containerd://", "", 1) // remove containerd:// prefix
+		containerIds = append(containerIds, containerID)
+	}
+
+	return containerIds
+}
+
+func getCrioPIDs(containerIDs []string, socketPath string) ([]int, error) {
+	var pids []int
+	for _, containerID := range containerIDs {
+		pid, err := getCrioPID(containerID, socketPath)
+		if err != nil {
+			return nil, err
+		}
+		pids = append(pids, pid)
+	}
+	return pids, nil
+}
+
+func getCrioPID(containerID string, socketPath string) (int, error) {
+	var pid int
+	cmd := exec.Command("sudo", "crictl", "-i", fmt.Sprintf("unix://%s", socketPath), "-r", fmt.Sprintf("unix://%s", socketPath), "inspect", containerID)
+	fmt.Printf("cmd: %s\n", cmd.String())
+	out, err := inspect(cmd)
+	if err != nil {
+		fmt.Printf("error on crictl run: %s\n", err.Error())
+		return 0, err
+	}
+
+	var resp CrictlInspectResponse
+	if err := json.Unmarshal(out, &resp); err != nil {
+		fmt.Printf("error on unmarshal crictl response: %s\n", err.Error())
+		return 0, err
+	}
+	pid = resp.Info.PID
+
+	return pid, nil
+}
+
+func inspect(cmd *exec.Cmd) ([]byte, error) {
+	var out, stdErr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stdErr
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+	return out.Bytes(), nil
 }
